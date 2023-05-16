7aa331edcb3d3be554341aac885be02c73f73692
0Omaaar <ellhotriomar@gmail.com>
2023-05-16 17:27:47 +0100

    added scripts to index

--- PFEProject/resources/views/base.blade.php
+++ PFEProject/resources/views/base.blade.php
@@ -3,21 +3,42 @@
 
 <head>
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/jquery-ui.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/nice-select.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/nice-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body>
 
     @include('includes.header')
     @yield('content')
+
+
+
+
+    <a href="#" id="scroll-top"><i class="far fa-arrow-up"></i></a>
+
+
+    <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/modernizr.min.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/imagesloaded.pkgd.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.appear.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('assets/js/counter-up.js')}}"></script>
+    <script src="{{asset('assets/js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 
 </body>
 
--- PFEProject/resources/views/includes/formulaire_recherche.blade.php
+++ PFEProject/resources/views/includes/formulaire_recherche.blade.php
@@ -32,14 +32,14 @@
                 <div class="col-lg-3">
                     <div class="form-group">
                         <label for="prix_min">Prix minimal</label>
-                        <input type="number" name="prix_min" id="prix_min" min="0">
+                        <input type="number" class="nice-select" name="prix_min" id="prix_min" min="0" >
                     </div>
                 </div>
 
                 <div class="col-lg-3">
                     <div class="form-group">
                         <label for="prix_max">Prix maximale</label>
-                        <input type="number" name="prix_max" id="prix_max" min="0">
+                        <input type="number" class="nice-select" name="prix_max" id="prix_max" min="0">
                     </div>
                 </div>
 
@@ -131,7 +131,6 @@
                 </div>
 
                 <div class="col-lg-3 align-self-end">
-                <i class="fas fa-volume-up"></i>
                     <button class="theme-btn" type="submit"><span class="far fa-search"></span>Lancer la recherche</button>
                 </div>
 

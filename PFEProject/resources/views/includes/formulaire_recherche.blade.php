6d17bee19503faff684fb0bb4ce77f94750caab9
0Omaaar <ellhotriomar@gmail.com>
2023-05-16 17:32:15 +0100

    modified just css

--- PFEProject/resources/views/includes/formulaire_recherche.blade.php
+++ PFEProject/resources/views/includes/formulaire_recherche.blade.php
@@ -1,4 +1,3 @@
-<div class="container">
     <div class="find-car-form">
         <h4 class="find-car-title">Trouvez votre voiture comme vous la désirez</h4>
         <form action="{{ route('annonces.recherche') }}" method="GET">
@@ -10,7 +9,7 @@
                         <select class="select" name="marque_id" id="marque_id" onchange="filterModels()">
                             <option value="">Toutes les marques</option>
                             @foreach ($marques as $marque)
-                            <option value="{{ $marque->id }}">{{ $marque->nom }}</option>
+                                <option value="{{ $marque->id }}">{{ $marque->nom }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -22,9 +21,9 @@
                         <select class="select" name="modele_id" id="modele_id">
                             <option value="">Tous les modèles Model</option>
                             @foreach ($modeles as $modele)
-                            <option value="{{ $modele->id }}" data-marque="{{ $modele->marque_id }}">
-                                {{ $modele->nom }}
-                            </option>
+                                <option value="{{ $modele->id }}" data-marque="{{ $modele->marque_id }}">
+                                    {{ $modele->nom }}
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -32,7 +31,7 @@
                 <div class="col-lg-3">
                     <div class="form-group">
                         <label for="prix_min">Prix minimal</label>
-                        <input type="number" class="nice-select" name="prix_min" id="prix_min" min="0" >
+                        <input type="number" class="nice-select" name="prix_min" id="prix_min" min="0">
                     </div>
                 </div>
 
@@ -49,7 +48,7 @@
                         <select class="select" name="ville" id="ville">
                             <option value="">Toutes les villes</option>
                             @foreach (['agadir', 'ait benhaddou', 'ait daoud', 'ait ourir', 'azrou', 'ben slimane', 'benguerir', 'beni mellal', 'berkane', 'berrechid', 'bouskoura', 'bouznika', 'casablanca', 'chefchaouen', 'chemaia', 'chichaoua', 'dakhla', 'dar bouazza', 'demnate', 'el hajeb', 'el jadida', 'errachidia', 'essaouira', 'fes', 'ifrane', 'kenitra', 'khemis zemamra', 'khemisset', 'larache', 'marrakech', 'martil', 'meknes', 'midelt', 'mohammedia', 'moulay bousselham', 'ouarzazate', 'ouezzane', 'oujda', 'settat', 'sidi slimane', 'tanger', 'taounate', 'taznakht', 'temara', 'tetouan'] as $option)
-                            <option value="{{ $option }}">{{ $option }}</option>
+                                <option value="{{ $option }}">{{ $option }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -87,7 +86,7 @@
                             <select class="select" name="puissance_fiscale" id="puissance_fiscale">
                                 <option value="">Toutes les puissances</option>
                                 @foreach ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60] as $option)
-                                <option value="{{ $option }}">{{ $option }}</option>
+                                    <option value="{{ $option }}">{{ $option }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -99,7 +98,7 @@
                             <select class="select" name="type" id="type">
                                 <option value="">Toutes les puissances</option>
                                 @foreach (['CABRIOLET', 'SUV ET 4X4', 'COUPé', 'CITADINE', 'BREAK', 'MONOSPACE', 'BERLINE', 'CC', 'MICRO-CITADINE', 'COMPACT', 'CROSSOVER', 'PICK UP', 'UTILITAIRE (MINIVAN)', 'UTILITAIRE (VAN)'] as $option)
-                                <option value="{{ $option }}">{{ $option }}</option>
+                                    <option value="{{ $option }}">{{ $option }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -111,7 +110,7 @@
                             <select class="select" name="annee_max" id="annee_max">
                                 <option value="">Toutes les années</option>
                                 @foreach (['2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990'] as $option)
-                                <option value="{{ $option }}">{{ $option }}</option>
+                                    <option value="{{ $option }}">{{ $option }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -123,7 +122,7 @@
                             <select class="select" name="annee_min" id="annee_min">
                                 <option value="">Toutes les années</option>
                                 @foreach (['2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990'] as $option)
-                                <option value="{{ $option }}">{{ $option }}</option>
+                                    <option value="{{ $option }}">{{ $option }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -131,7 +130,8 @@
                 </div>
 
                 <div class="col-lg-3 align-self-end">
-                    <button class="theme-btn" type="submit"><span class="far fa-search"></span>Lancer la recherche</button>
+                    <button class="theme-btn" type="submit"><span class="far fa-search"></span>Lancer la
+                        recherche</button>
                 </div>
 
                 <div id="btn-recherche-avancee">
@@ -144,7 +144,6 @@
             </div>
         </form>
     </div>
-</div>
 
 <script>
     document.getElementById('btn-recherche-avancee').addEventListener('click', function() {

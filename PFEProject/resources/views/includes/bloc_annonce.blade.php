5ef3348d5e65bb9d913e8868b58dd88b1d71fa08
emaddaem <emaddaem@gmail.com>
2023-05-18 15:07:51 +0100

    j'ai mis le script qui affiche le num telephone a la place du prix dans une fonction javascript

--- PFEProject/resources/views/includes/bloc_annonce.blade.php
+++ PFEProject/resources/views/includes/bloc_annonce.blade.php
@@ -15,8 +15,9 @@
                 </ul>
                 <div class="car-footer">
                     @if ($annonce->prix == null)
-                        <p id="appeler-prix" class="car-price"><strong><a href="#"
-                                    style="text-decoration: none;">Appelez pour le prix</a></strong></p>
+                        <p id="appeler-prix" class="car-price">
+                            <strong><a href="#" onclick="afficherNumero()" style="text-decoration: none;">Appelez pour le prix</a></strong>
+                        </p>
                         <p id="tel" class="car-price" style="display:none;">
                             <strong>{{ $annonce->user->telephone }}</strong>
                         </p>

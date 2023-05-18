676a12137a6faa68721aead0768a761375132f9a
0Omaaar <ellhotriomar@gmail.com>
2023-05-18 15:52:27 +0100

    upadted liste des marques

--- PFEProject/app/Http/Controllers/AnnonceController.php
+++ PFEProject/app/Http/Controllers/AnnonceController.php
@@ -18,7 +18,7 @@ class AnnonceController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'show']);
+        $this->middleware('auth')->except(['index', 'show', 'parMarque']);
     }
 
 

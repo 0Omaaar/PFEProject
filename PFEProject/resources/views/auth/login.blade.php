0fcf61a514c113742651b1e6b63b38584edb0cee
0Omaaar <ellhotriomar@gmail.com>
2023-05-22 17:23:43 +0100

    updated login and register

--- PFEProject/resources/views/auth/login.blade.php
+++ PFEProject/resources/views/auth/login.blade.php
@@ -1,42 +1,40 @@
 @extends('base')
 @section('content')
-    <main class="login-form">
-        <div class="cotainer">
-            <div class="row justify-content-center">
-                <div class="col-md-4">
-                    <div class="card">
-                        <h3 class="card-header text-center">Login</h3>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login.custom') }}">
-                                @csrf
-                                <div class="form-group mb-3">
-                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
-                                           autofocus>
-                                    @if (isset($errors) && $errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                                    @if (isset($errors) && $errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Signin</button>
-                                </div>
-                            </form>
+    <div class="login-area">
+        <div class="container">
+            <div class="col-md-5 mx-auto">
+                <div class="login-form">
+                    <div class="login-header">
+                        <p>S'authentifier</p>
+                    </div>
+                    <form method="POST" action="{{ route('login.custom') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" placeholder="Email" id="email" class="form-control" name="email"
+                                required autofocus>
+                            @if (isset($errors) && $errors->has('email'))
+                                <span class="text-danger">{{ $errors->first('email') }}</span>
+                            @endif
                         </div>
+                        <div class="form-group">
+                            <label>Mot de passe</label>
+                            <input type="password" placeholder="Password" id="password" class="form-control"
+                                name="password" required>
+                            @if (isset($errors) && $errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                            @endif
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <button type="submit" class="theme-btn">S'authentifier<i class="mdi mdi-login-variant"></i></button>
+                        </div>
+                    </form>
+                    <div class="login-footer">
+                        <p>Vous n'avez pas de compte ?
+                            <a href="{{route('register-user')}}">Créer un compte</a></p>
                     </div>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
 @endsection
--- PFEProject/resources/views/auth/registration.blade.php
+++ PFEProject/resources/views/auth/registration.blade.php
@@ -1,63 +1,73 @@
 @extends('base')
 @section('content')
-<main class="signup-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-4">
-                <div class="card">
-                    <h3 class="card-header text-center">Register User</h3>
-                    <div class="card-body">
-                        <form action="{{ route('register.custom') }}" method="POST">
-                            @csrf
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Nom" id="nom" class="form-control" name="nom" required autofocus>
-                                @if (isset($errors) && $errors->has('nom'))
+    <div class="login-area">
+        <div class="container">
+            <div class="col-md-5 mx-auto">
+                <div class="login-form">
+                    <div class="login-header">
+                        <p>Créer un compte</p>
+                    </div>
+                    <form action="{{ route('register.custom') }}" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <label>Nom</label>
+                            <input type="text" placeholder="Nom" id="nom" class="form-control" name="nom"
+                                required autofocus>
+                            @if (isset($errors) && $errors->has('nom'))
                                 <span class="text-danger">{{ $errors->first('nom') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Prénom" id="prenom" class="form-control" name="prenom" required autofocus>
-                                @if (isset($errors) && $errors->has('prenom'))
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="Prénom">Prénom</label>
+                            <input type="text" placeholder="Prénom" id="prenom" class="form-control" name="prenom"
+                                required autofocus>
+                            @if (isset($errors) && $errors->has('prenom'))
                                 <span class="text-danger">{{ $errors->first('prenom') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Email" id="email_address" class="form-control" name="email" required autofocus>
-                                @if (isset($errors) && $errors->has('email'))
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="text" placeholder="Email" id="email_address" class="form-control" name="email"
+                                required autofocus>
+                            @if (isset($errors) && $errors->has('email'))
                                 <span class="text-danger">{{ $errors->first('email') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="number" placeholder="Numéro de téléphone" id="telephone" class="form-control" name="telephone" required autofocus>
-                                @if (isset($errors) && $errors->has('telephone'))
+                            @endif
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="telephone">Téléphone</label>
+                            <input type="number" placeholder="Numéro de téléphone" id="telephone" class="form-control"
+                                name="telephone" required autofocus>
+                            @if (isset($errors) && $errors->has('telephone'))
                                 <span class="text-danger">{{ $errors->first('telephone') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="text" placeholder="Ville" id="ville" class="form-control" name="ville" required autofocus>
-                                @if (isset($errors) && $errors->has('ville'))
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="ville">Ville</label>
+                            <input type="text" placeholder="Ville" id="ville" class="form-control" name="ville"
+                                required autofocus>
+                            @if (isset($errors) && $errors->has('ville'))
                                 <span class="text-danger">{{ $errors->first('ville') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                                @if (isset($errors) && $errors->has('password'))
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input type="password" placeholder="Password" id="password" class="form-control"
+                                name="password" required>
+                            @if (isset($errors) && $errors->has('password'))
                                 <span class="text-danger">{{ $errors->first('password') }}</span>
-                                @endif
-                            </div>
-                            <div class="form-group mb-3">
-                                <div class="checkbox">
-                                    <label><input type="checkbox" name="remember"> Remember Me</label>
-                                </div>
-                            </div>
-                            <div class="d-grid mx-auto">
-                                <button type="submit" class="btn btn-dark btn-block">Sign up</button>
-                            </div>
-                        </form>
+                            @endif
+                        </div>
+                        <div class="d-flex align-items-center">
+                            <button type="submit" class="theme-btn">Créer<i class="mdi mdi-login-variant"></i></button>
+                        </div>
+                    </form>
+                    <div class="login-footer">
+                        <p>Vous avez déjà un compte ?
+                            <a href="{{ route('login') }}">S'authentifier</a>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</main>
 @endsection

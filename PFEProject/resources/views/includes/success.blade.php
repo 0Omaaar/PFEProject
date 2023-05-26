66c35843b5911970d9ac605c182c5ef8ff11cff3
0Omaaar <ellhotriomar@gmail.com>
2023-05-26 10:11:20 +0100

    updated success div

--- PFEProject/resources/views/includes/success.blade.php
+++ PFEProject/resources/views/includes/success.blade.php
@@ -1,15 +1,14 @@
 @if (session()->has('success'))
-    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 0px;">
-        <div class="toast bg-light" role="alert" aria-live="assertive" aria-atomic="true"
-            style="position: absolute; top: 0px; right: 0px;">
+    <div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true"
+            style="border-Left: 5px solid green ; position: absolute; top: 20px; right:30px   ">
             <div class="toast-header">
                 <strong class="mr-auto">Succes</strong>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close"
+                <button type="button" class="fa fa-times close" data-dismiss="toast" aria-label="Close"
                     style="margin-left: 250px;">
-                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="toast-body">
+            <div class="toast-body fw-bolder">
                 {{ session()->get('success') }}
             </div>
         </div>
